b8f20c1938448482c0fc76a901b388c7cec3a285
Alliana <56180684+Allianaab2m@users.noreply.github.com>
2021-08-22 19:17:37 +0900

    add: no_report.py

--- sheets/sheet.py
+++ sheets/sheet.py
@@ -5,7 +5,7 @@ import const
 
 scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
 
-credentials = ServiceAccountCredentials.from_json_keyfile_name('sheets/gcpkey.json')
+credentials = ServiceAccountCredentials.from_json_keyfile_name('.data/gcpkey.json')
 
 gc = gspread.authorize(credentials)
 SPREADSHEET_KEY = const.SPREADSHEET_KEY
