85295a11d004f92f4b713600556152b38a2095a6
Alliana <56180684+Allianaab2m@users.noreply.github.com>
2021-09-07 20:58:47 +0900

    エラー内容を表示するように変更

--- cogs/command_error_handler.py
+++ cogs/command_error_handler.py
@@ -1,4 +1,6 @@
-from discord.ext.commands import Bot, CheckFailure, Cog, CommandError, CommandNotFound, Context
+from discord.ext.commands import Bot, CheckFailure, Cog, CommandError, CommandNotFound, Context, \
+    errors
+import discord
 
 
 class CommandErrorHandler(Cog):
@@ -12,7 +14,12 @@ class CommandErrorHandler(Cog):
         ignore_errors = (CheckFailure, CommandNotFound)
         if isinstance(error, ignore_errors):
             return
-        await ctx.reply(error)
+        error_embed: discord.Embed = discord.Embed(title="エラー",
+                                                   description="エラーが発生しました。\n"
+                                                               "このエラーが複数回発生する場合は，直るまで気長に待ってください。",
+                                                   color=0xff0000)
+        error_embed.add_field(name="エラー内容", value=str(error))
+        await ctx.reply(embed=error_embed)
 
 
 def setup(bot: Bot) -> None:
